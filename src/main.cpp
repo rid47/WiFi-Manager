f78f3e191a82f02d67964ebff196334113abbc23
rid47 <ridwanmizan@gmail.com>
2018-04-22 12:02:14 +0600

    Bare minimum for wifi manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-#include <Arduino.h>
-# define ledpin 2
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>        // https://github.com/tzapu/WiFiManager
+
 void setup() {
-    // put your setup code here, to run once:
 
-
+  Serial.begin(115200);
+  WiFiManager wifiManager;
+  wifiManager.resetSettings();
+  wifiManager.autoConnect("Mushroom Device", "admin1234");
+  Serial.println("Connected.");
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
-}
+void loop(){}
